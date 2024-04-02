74c73690f39d2cea2b01b7711c2abe675f12ab8b
Kavindu N <kavindu@digitcave.com>
2024-04-03 00:56:36 +0530

    Developed Login page layout

--- index.php
+++ index.php
@@ -3,9 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Napronet E System</title>
+    <link rel="stylesheet" href="assets/Css/index.css">
+    <script src="https://kit.fontawesome.com/0df6f8475c.js" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    
+
+    <div class="container">
+        <div class="loginheader">
+            <img src="/assets/images/logo-1-192x138.png" alt="" id="logo" width="250px" height="200px">
+            <h1>Welcome</h1>
+            <h2>Napronet Pvt Ltd E-Management System</h2>
+        </div>
+
+        <div class="loginbody">
+            <form action="/includes/login.php">
+                <div id="usericon"><i class="fa-solid fa-user"></i></div>
+                <label for="username" class="loginformlabel">Enter Your User Name</label>
+                <input type="email" name="username" id="">
+                <div class="loginpassword">
+                    <label for="password" class="loginformlabel">Enter Your Password</label>
+                    <input type="password" name="password" id="">
+                </div>
+                <button id="login-button">Login</button>
+            </form>
+        </div>
+    </div>
+
+
+
+    <script src="javascript/login.js"></script>
 </body>
 </html>
