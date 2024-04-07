dcdc422fe965e7726f4439a0e313e92c72631888
Kavindu N <kavindu@digitcave.com>
2024-04-07 12:26:35 +0530

    Modified Dashboard layout

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,31 @@
     <title>Dashboard</title>
 </head>
 <body>
+    <div class="header">
+        <div class="logo"><img src="/assets/images/logo-1-192x138.png" alt="" height="100px" width="120px"></div>
+        <button class="custom-btn btn-1">Stock Overview</button>
+        <button class="custom-btn btn-1">Stock Update</button>
+        <button class="custom-btn btn-1">Attendance</button>
+        <button class="custom-btn btn-1">Order Management</button>
+        <button class="custom-btn btn-1">Inventory</button>
+    </div>
+
+    <div class="overview-wrapper">
+        <div class="card_box">
+            <div class="time">
+                <img src="assets/images/clock.png" alt="" height="150px" width="150px" id="clockpng">
+                <h3 class="time"><b>Today</b></h3>
+                <h4 class="current-time">11.35 Am</h4>
+                <p id="date">Tuesday 24th March 2024 </p>   
+            </div>
+                        
+            
+        </div>
+</div>
+
+        </div>
+
+    </div>
 
 
 
