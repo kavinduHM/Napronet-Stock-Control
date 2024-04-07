b961dc0902a095fe7b571cbea2e7f6b63a783a16
Kavindu N <kavindu@digitcave.com>
2024-04-07 11:13:49 +0530

    Created Dashboard markup

--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/Css/dashboard.css">
     <title>Dashboard</title>
 </head>
 <body>
+
+
+
+
+
+
     
 </body>
 </html>
+
