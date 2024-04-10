fcfd13060908769d835ed17700a42c9ed1f8c6fe
Kavindu N <kavindunethsara445@gmail.com>
2024-04-10 07:58:11 +0530

    Employer manager dashboard layout

---
+++ emmanager.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/Css/emmanager.css">
+    <title>Employer Management</title>
+</head>
+<body>
+
+    <div class="header nav">
+        <button class="custom-btn btn-1">Stock Overview</button>
+        <button class="custom-btn btn-1">Employee Manager</button>
+        <button class="custom-btn btn-1">Attendance</button>
+        <button class="custom-btn btn-1">Order Management</button>
+        <button class="custom-btn btn-1">Inventory</button>
+    </div>
+
+</body>
+</html>
