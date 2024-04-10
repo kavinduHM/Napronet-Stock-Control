cbca6845dfc33ed36c82d794ac10a3ab81d94d40
Kavindu N <kavindunethsara445@gmail.com>
2024-04-10 09:50:52 +0530

    Markup of Employer management page

--- emmanager.php
+++ emmanager.php
@@ -8,12 +8,61 @@
 </head>
 <body>
 
-    <div class="header nav">
-        <button class="custom-btn btn-1">Stock Overview</button>
-        <button class="custom-btn btn-1">Employee Manager</button>
-        <button class="custom-btn btn-1">Attendance</button>
-        <button class="custom-btn btn-1">Order Management</button>
-        <button class="custom-btn btn-1">Inventory</button>
+    <div class="header">
+        <a href="/dashboard.php"><button class="custom-btn btn-1">Stock Overview</button></a>
+        <a href="/emmanager.php"><button class="custom-btn btn-1">Employee Manager</button></a>
+        <a href="/attendance.php"><button class="custom-btn btn-1">Attendance</button></a>      
+        <a href="/ordermanagement.php"><button class="custom-btn btn-1">Order Management</button></a>
+        <a href="/inventory.php"><button class="custom-btn btn-1">Inventory</button></a>
+        <div class="logo"><img src="/assets/images/logo-1-192x138.png" alt="" height="100px" width="120px"></div>
+    </div>
+
+    <div class="heading">
+        <div class="blank"></div>
+        <div class="head-text">
+            <img src="/assets/images/empimage.png" alt="" height="175px" width="190px">
+            <h2>Employer Management System</h2>
+            <p>View / Add / Remove / Edit Employer Details</p>
+        </div>
+    </div>
+
+    <div class="current-emp">
+        <div class="permanant-emp">
+            <table class="permanant-em-table">
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Contact Number</th>
+                    <th>Department</th>
+                    <th>Position</th>
+                    <th>Pay / Per day</th>
+                    <tr>
+                        <td>S.P.Chamila Niroshini</td>
+                        <td>374/2,Paragammana,Yakwila</td>
+                        <td>077-852456888</td>
+                        <td>Production</td>
+                        <td>Supervisor</td>
+                        <td>Rs.1250</td>
+                    </tr>
+                    <tr>
+                        <td>W.A.Rasika Arachchige</td>
+                        <td>374/2,Dabagahagedara,Yakwila</td>
+                        <td>077-425669875</td>
+                        <td>Production</td>
+                        <td>Mechanic</td>
+                        <td>Rs.2500</td>
+                    </tr>
+                    <tr>
+                        <td>A.P.Thanuja Gunarathna</td>
+                        <td>Japan House Scheme,Paragammana,Yakwila</td>
+                        <td>077-425669875</td>
+                        <td>Production</td>
+                        <td>Machine operator</td>
+                        <td>Rs.850</td>
+                    </tr>
+                    
+                
+            </table>
+        </div>
     </div>
 
 </body>
